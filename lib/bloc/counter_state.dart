a6c96c99f40f5503e0cfedc89aedfd06ee49f766
Sam Espinoza <sespino3@stumail.jccc.edu>
2022-12-13 18:49:05 -0600

    completed hydrated bloc

---
+++ lib/bloc/counter_state.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'counter_bloc.dart';
+
+class CounterState extends Equatable {
+  final int counter;
+  CounterState({
+    required this.counter,
+  });
+
+  factory CounterState.initial() {
+    return CounterState(counter: 0);
+  }
+
+  @override
+  List<Object> get props => [counter];
+
+  @override
+  String toString() => 'CounterState(counter: $counter)';
+
+  CounterState copyWith({
+    int? counter,
+  }) {
+    return CounterState(
+      counter: counter ?? this.counter,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'counter': counter,
+    };
+  }
+
+  factory CounterState.fromMap(Map<String, dynamic> map) {
+    return CounterState(
+      counter: map['counter'] as int,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory CounterState.fromJson(String source) =>
+      CounterState.fromMap(json.decode(source) as Map<String, dynamic>);
+}
